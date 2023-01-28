191f20fcea20489bd61d0bd747836e2958de173b
ThomasAndon <513230863@qq.com>
2023-01-28 22:29:59 +0800

    1

--- service/main/internal/logic/bondlogic.go
+++ service/main/internal/logic/bondlogic.go
@@ -2,7 +2,6 @@ package logic
 
 import (
 	"context"
-
 	"dining_home_backend_newest/service/main/internal/svc"
 	"dining_home_backend_newest/service/main/internal/types"
 
@@ -25,6 +24,7 @@ func NewBondLogic(ctx context.Context, svcCtx *svc.ServiceContext) *BondLogic {
 
 func (l *BondLogic) Bond(req *types.BondRequest) (resp *types.CommonResponse, err error) {
 	// todo: add your logic here and delete this line
-
+	a := l.ctx.Value("mwKey")
+	print(a)
 	return
 }
--- service/main/internal/middleware/globalmiddleware.go
+++ service/main/internal/middleware/globalmiddleware.go
@@ -12,7 +12,7 @@ func NewGlobalMiddleware() *GlobalMiddleware {
 func (m *GlobalMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO generate middleware implement function, delete after code implementation
-
+		r.Header.Add("mwKey", "mwValue")
 		// Passthrough to next handler if need
 		next(w, r)
 	}
